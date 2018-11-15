eb654b2e79ed3f153f0c49053915496d8d017e70
SUN YILONG <YSUN232@HOTMAIL.COM>
2018-11-14 22:02:58 -0500

    prints five times

---
+++ fivetimes.py
@@ -0,0 +1,16 @@
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times('+str(i) +')')
+
+total = 0
+for num in range(101):
+    total= total +num
+    if total== 2346:
+        print('nice try kid, but I gotta stop you right here')
+        break
+    print(total)
+#to make an infinite loop, just start with while True
+#to manipulate anything in the while loop, just use
+    #continue to get back to the start of the function or
+    #break to get out of the loop
+    #Use control + C to get out of the execution of a module in IDLE
