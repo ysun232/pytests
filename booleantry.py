444130dbcd56fef99fc53315a0548da356da98cf
SUN YILONG <YSUN232@HOTMAIL.COM>
2018-11-14 22:01:50 -0500

    adding boolean

---
+++ booleantry.py
@@ -0,0 +1,13 @@
+print('Welcome to Yilongs first test to conditions and booleans')
+name = ''
+while name != 'Yilong':
+    print('Please input my name and it will give you access to this file')
+    name=input()
+    if name == 'Yilong':
+        print('Access granted')
+        print('Welcome to my world, now the program will end')
+    elif name== 'Yilongs':
+        print('Close but still need to learn to read, try again')
+    else:
+        print('Learn to read nerd :^), try again')
+print('End of program')
